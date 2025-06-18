0710a1564b9f6a37aa7d822ef100499f32fb8662
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2025-06-18 14:58:00 +0200

    Init repo

---
+++ src/data/services.js
@@ -0,0 +1,34 @@
+const services = [
+  {
+    icon: '🚀',
+    title: 'Custom Web Design and Development',
+    text: "We create unique and functional websites, designed from scratch to reflect your brand's identity and fulfill your specific goals.",
+  },
+  {
+    icon: '📈',
+    title: 'SEO-Optimized Websites',
+    text: "We develop your website with the best SEO practices, ensuring it's visible in search engines and attracts your target audience.",
+  },
+  {
+    icon: '📱',
+    title: 'Responsive and Mobile-First Design',
+    text: 'Your website will look and perform perfectly on any device, from desktops to tablets and smartphones, ensuring an excellent user experience.',
+  },
+  {
+    icon: '🛍️',
+    title: 'Online Stores and E-commerce',
+    text: 'Boost your sales with a robust and user-friendly online store, designed to convert visitors into customers and manage your products efficiently.',
+  },
+  {
+    icon: '🔧',
+    title: 'Website Maintenance and Support',
+    text: 'We offer continuous maintenance services, security updates, backups, and technical support to keep your website up-to-date and running smoothly.',
+  },
+  {
+    icon: '⚡',
+    title: 'Performance and Speed Optimization',
+    text: "We improve your website's loading speed to offer a better user experience and better search engine ranking, reducing bounce rate.",
+  },
+]
+
+export default services
---
+++ src/data/works.js
@@ -0,0 +1,32 @@
+const companies = [
+  {
+    name: 'Recambios Las Margaritas',
+    url: 'https://recambioslasmargaritas.es/',
+    category: 'Automotive',
+    description: 'Company dedicated to the sale of automotive spare parts.',
+    cover: 'recambios.webp',
+  },
+  {
+    name: 'Baterías Express',
+    url: 'https://www.bateriascoches24horas.es/',
+    category: 'Automotive',
+    description: '24-hour car battery sales and installation service.',
+    cover: 'baterias.webp',
+  },
+  {
+    name: 'Ziamari Construcciones',
+    url: 'https://www.ziamariconstruccion.com/',
+    category: 'Construction',
+    description: 'Construction company specialized in various projects.',
+    cover: 'ziamari.webp',
+  },
+  {
+    name: 'Barreto Construcciones',
+    url: 'https://barretoconstrucciones.es/',
+    category: 'Construction',
+    description: 'Construction company with experience in building and renovations.',
+    cover: 'barreto.webp',
+  },
+]
+
+export default companies
